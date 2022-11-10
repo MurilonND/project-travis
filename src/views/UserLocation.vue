50d5866783080bc2db7254a0ba0cb89d197a69c5
MuriloND <murilonog2013@gmail.com>
2022-11-09 21:36:37 -0300

    feature/strating set up

---
+++ src/views/UserLocation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <section id="form">Form</section>
+    <button id="button">Icon</button>
+
+    <section id="map">Map</section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
