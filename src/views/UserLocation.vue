259b1958f562a6996582df76a09ea2f783dc0b50
MuriloND <murilonog2013@gmail.com>
2022-11-10 14:01:46 -0300

    feature/buttons functions

--- src/views/UserLocation.vue
+++ src/views/UserLocation.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
     <section id="form">Form</section>
-    <button id="button">Icon</button>
+    <button id="button-open" class="Hide" @click="OpenButton()">
+      IconOpen
+    </button>
+    <button id="button-close" @click="CloseButton()">IconClose</button>
 
     <section id="map">Map</section>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    OpenButton() {
+      document.getElementById("button-open").classList.add("Hide");
+      document.getElementById("button-close").classList.remove("Hide");
+      document.getElementById("form").classList.remove("Hide");
+    },
+    CloseButton() {
+      document.getElementById("button-close").classList.add("Hide");
+      document.getElementById("button-open").classList.remove("Hide");
+      document.getElementById("form").classList.add("Hide");
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+.Hide {
+  display: none;
+}
+</style>
